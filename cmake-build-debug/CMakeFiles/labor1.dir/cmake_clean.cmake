b9df9bf812f9786ef32eefe668d38a9ff7ffb6bf
kati1989 <56889441+kati1989@users.noreply.github.com>
2021-02-22 21:36:49 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/labor1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labor1.dir/main.c.obj"
+  "labor1.exe"
+  "labor1.exe.manifest"
+  "labor1.pdb"
+  "liblabor1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/labor1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
